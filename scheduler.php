46851bacf2a3477f10e21e523f87dc575034b87e
unknown <joshita1999@gmail.com>
2020-02-18 21:16:37 -0700

    Added to form

--- scheduler.php
+++ scheduler.php
@@ -1,5 +1,4 @@
 Your patient is:
 <?php echo htmlspecialchars($_POST['firstName']); ?>.
 <?php echo htmlspecialchars($_POST['lastName']); ?>.
-<?php echo htmlspecialchars($_POST['newPatient']); ?>.
-<?php echo (int)$_POST['age']; ?> years old.
+<?php echo htmlspecialchars($_POST['timeConstraints']); ?>.
